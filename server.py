285c421b34039e12c7e8b1332e83739d78d5f58b
amir <AmirRezaFahimi98@gmail.com>
2020-10-18 12:19:26 +0330

    Initial Commit

--- server.py
+++ server.py
@@ -1,20 +1,8 @@
 from flask import Flask, render_template, \
     request, redirect
-import csv
-import smtplib
-import statics
-from flask_mail import Mail, Message
+import spreadsheet
 
 app = Flask(__name__)
-app.config.update(
-    MAIL_SERVER='smtp.gmail.com',
-    MAIL_PORT=465,
-    MAIN_USE_SSL=True,
-    MAIN_USERNAME=statics.username,
-    MAIN_PASSWORD=statics.password
-)
-mail = Mail(app)
-print(__name__)
 
 
 @app.route('/')
@@ -27,37 +15,12 @@ def html_page(page_name):
     return render_template(page_name)
 
 
-# def write_to_file(data):
-#     with open('database.txt', mode='a') as database:
-#         email = data['email']
-#         subject = data['subject']
-#         message = data['message']
-#         file = database.write(f'\n{email},{subject},{message}')
-
-
-def write_to_csv(data):
-    with open('database.csv', mode='a') as database:
-        email = data['email']
-        subject = data['subject']
-        message = data['message']
-        csv_writer = csv.writer(database, newline='', delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow([email, subject, message])
-
-
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
-        send_email(data)
-        # write_to_file(data)
-        # write_to_csv(data)
+        csv_writer = spreadsheet.WriteToSpreadSheet(data)
+        csv_writer.write_data()
         return redirect('/thankyou.html')
     else:
         return 'something went wrong'
-
-
-def send_email(data):
-    msg = mail.send(
-        message=data['email'] + '\n' + data['subject'] + '\n' + data['message']
-    )
-    return msg
