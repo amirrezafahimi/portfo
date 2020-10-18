9a6c456cb5e179a178aaae5e8c36afedce1b30da
amir <AmirRezaFahimi98@gmail.com>
2020-10-18 12:19:41 +0330

    Initial Commit

---
+++ statics.py
@@ -0,0 +1,5 @@
+username = 'amirrezafahimi98@gmail.com'
+password = 'zfrqftocwhxjdkdu'
+
+client_id = '856208007086-tj2jm6ra68npn29kqntuo53p4513r7nv.apps.googleusercontent.com'
+client_secret = 'qCU-3_yPcixlAXRcxYtavPiK'
