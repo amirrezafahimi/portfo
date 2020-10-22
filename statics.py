a15c3c218c1cc48bd5ab60c4a318ab3a621eec3d
amirrezafahimi <amirrezafahimi98@gmail.com>
2020-10-23 00:43:53 +0330

    Initial Commit

--- statics.py
+++
@@ -1,5 +0,0 @@
-username = 'amirrezafahimi98@gmail.com'
-password = 'zfrqftocwhxjdkdu'
-
-client_id = '856208007086-tj2jm6ra68npn29kqntuo53p4513r7nv.apps.googleusercontent.com'
-client_secret = 'qCU-3_yPcixlAXRcxYtavPiK'
